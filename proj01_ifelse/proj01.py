7ab016a613e8342333255d7cb7a5b7ce9bfb1a80
LukumBest <40004965+LukumBest@users.noreply.github.com>
2018-06-12 09:59:40 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -22,7 +22,7 @@ print name + ", you will graduate from high school in " + str(years) + " years."
 print "End of Program 1."
 
 current_month = 6
-current_day = 11
+current_day = 12
 name_2 = raw_input("What's your name? ")
 name_2 = name_2[0:1].upper() + name_2[1:].lower()
 birth_month = int(raw_input("Enter the number of your birth month. "))
@@ -38,11 +38,41 @@ if birth_month < current_month:
         days_2 = int(30 - (current_day - birth_day))
     print name + ", your birthday is in " + str(months_2) + " months and " + str(days_2) + " days!"
     print "End of Program 2."
+    
+age = int(raw_input("How old are you? "))
+if age > 16:
+    print "You can see R rated movies. Great."
+elif age < 8:
+    print "You should be allowed to see PG movies."
+elif age < 14:
+    print "You should be allowed to see PG-13 movies."
+
+
+
 
 name_3 = raw_input("Hi friend! What's your name? ")
+if len(name_3) > 10:
+    print "That's a long name."
 print "My name's Macintosh."
-raw_input("Where are you from? ")
-if input()
+state = (raw_input("Where are you from? "))
+if state == "alabama" or state == "Alabama":
+    print "Neat."
+elif state == "alaska" or state == "Alaska":
+    print "The lights there are so beautiful! I've been a couple of times myself."
+elif state == "arizona" or state == "Arizona":
+    print "Neat."
+elif state == "arkansas" or state == "Arkansas":
+    print "Nice."
+elif state == "california" or state == "California":
+    city = raw_input("Where in California? ")
+    if city == "Las Vegas" or city == "las vegas" or city == "Las vegas" or city == "las Vegas" or city == "Vegas" or city == "vegas":
+        print "With the famed Vegas Strip, and the casinos and stuff? I've never been there."
+
+
+    
+
+
+
 
 
 
