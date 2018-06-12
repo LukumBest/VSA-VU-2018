4ac50908643d8453289a4aa80bfb3f2f76ca01aa
LukumBest <40004965+LukumBest@users.noreply.github.com>
2018-06-12 11:35:57 -0500

    Add files via upload

--- proj02_loops/proj02_01.py
+++ proj02_loops/proj02_01.py
@@ -17,3 +17,48 @@
 # Enter a number to sum, or 0 to indicate you are finished: 10
 # Enter a number to sum, or 0 to indicate you are finished: 0
 #The sum of your numbers is: 21
+sum = 0
+loop = 1
+while loop != 0:
+    num = int(raw_input("Enter a number to add, or enter 0 to finish. "))
+    sum = num + sum
+    if num == 0:
+        loop = loop - 1
+        print "The sum of the numbers is " + str(sum) +  " ."
+
+print " "
+game = 2
+print "Rock, Paper, Scissors"
+loop = 0
+while game == 2:
+    u_1 = raw_input("Player 1, enter either rock, paper, or scissors.")
+    u_2 = raw_input("Player 2, enter either rock, paper, or scissors.")
+    if u_1 == "rock" or "Rock":
+        if u_2 == "paper" or "Paper":
+            print "Player 2 wins!"
+    elif u_1 == "Rock" or "rock":
+        if u_2 == "scissors" or "Scissors":
+            print "Player 1 wins!"
+    elif u_1 == "rock" or "Rock":
+        if u_2 == "Rock" or "rock":
+            print "Tie!"
+    elif u_1 == "paper" or "Paper":
+        if u_2 == "rock" or "Rock":
+            print "Player 1 wins!"
+    elif u_1 == "paper" or "Paper":
+        if u_2 == "paper" or "Paper":
+            print "Tie!"
+    elif u_1 == "paper" or "Paper":
+        if u_2 == "scissors" or "Scissors":
+            print "Player 2 wins!"
+    elif u_1 == "scissors" or "Scissors":
+        if u_2 == "rock" or "Rock":
+            print "Player 2 wins!"
+    elif u_1 == "scissors" or "Scissors":
+        if u_2 == "paper" or "Paper":
+            print "Player 1 wins!"
+    elif u_1 == "scissors" or "Scissors":
+        if u_2 == "scissors" or "Scissors":
+            print "Tie!"
+
+raw_input("Would you like to play again?")
